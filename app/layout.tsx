9182c1e58aaa5f795f96c98c554fc229d182b8df
invisprints <invisprints@outlook.com>
2023-05-08 15:42:51 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Private Assistant",
+  description: "Personal Private Assistant.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Private Assistant",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
